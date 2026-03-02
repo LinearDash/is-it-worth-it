21cfa91ad7a64bc481815ce2346444f3e14ccf2c
LinearDash <asandesh495@gmail.com>
2026-03-02 17:10:50 +0545

    fix: update .gitignore to include sensitive files and initialize passport middleware

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ dontenv.config()
 
 const express = require('express')
 const cors = require('cors')
+const passport = require('./auth/passport')
 
 const app = express()
 const PORT = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
-
+app.use(passport.initialize())
 
 
 const router =require('./apis/routes')
